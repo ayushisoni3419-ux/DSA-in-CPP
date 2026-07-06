b9277a74937dc7257361e8da229580975c36605f
Ayushi Soni <ayushisoni3419@gmail.com>
2026-07-06 23:54:25 +0530

    Add Second Largest Array solution

---
+++ ARRAYS/SEC largest.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest)
+        {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == INT_MIN)
+        cout << "Second largest element does not exist.";
+    else
+        cout << "Second largest element = " << secondLargest;
+
+    return 0;
+}cle
